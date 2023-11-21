cdfde433edbd61433c864c312a10cdacbebd84dc
teresadkla <teresafernandeslbbh@gmail.com>
2023-11-21 04:07:51 +0000

    imgs proporcionais

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-/*document.addEventListener('DOMContentLoaded', function () {
-    var imageContainer = document.getElementById('intro');
-
-    document.addEventListener('click', function (event) {
-        // Create an image element
-        var img = document.createElement('img');
-
-        // Set the source of the image (this can be a URL or a local path)
-        img.src = 'images/poster.png';
-
-        // Set other attributes if needed (e.g., width, height, etc.)
-        img.width = 100;
-        img.height = 100;
-
-        // Set the position based on the click coordinates
-        img.style.position = 'absolute';
-        img.style.left = (event.clientX - img.width / 2) + 'px';
-        img.style.top = (event.clientY - img.height / 2) + 'px';
-
-        // Add the 'bounce' class to apply the animation
-        img.classList.add('bounce');
-
-        // Append the image to the container
-        imageContainer.appendChild(img);
-    });
-});*/
 document.addEventListener('DOMContentLoaded', function () {
     var imageContainer = document.getElementById('intro');
 
@@ -61,10 +35,6 @@ document.addEventListener('DOMContentLoaded', function () {
         // Set the source of the image
         img.src = randomImageSource;
 
-        // Set other attributes if needed (e.g., width, height, etc.)
-        img.width = 100;
-        img.height = 100;
-
         // Set the position based on the click coordinates
         img.style.position = 'absolute';
         img.style.left = (event.clientX - img.width / 2) + 'px';
@@ -73,7 +43,26 @@ document.addEventListener('DOMContentLoaded', function () {
         // Add the 'bounce' class to apply the animation
         img.classList.add('bounce');
 
-        // Append the image to the container
-        imageContainer.appendChild(img);
+        // Load the image to get its natural width and height
+        img.onload = function () {
+            // Calculate the aspect ratio
+            var aspectRatio = img.width / img.height;
+
+            // Set the width and height based on the aspect ratio
+            if (aspectRatio >= 1) {
+                img.style.width = '300px';
+                img.style.height = 'auto';
+            } else {
+                img.style.width = 'auto';
+                img.style.height = '300px';
+            }
+
+            // Update the position based on the click coordinates
+            img.style.left = (event.clientX - img.clientWidth / 2) + 'px';
+            img.style.top = (event.clientY - img.clientHeight / 2) + 'px';
+
+            // Append the image to the container
+            imageContainer.appendChild(img);
+        };
     });
 });
